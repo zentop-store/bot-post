eaa05318abba2ee11319e5276c26c0e30ea18810
zentop <dedesans12@gmail.com>
2024-09-11 21:51:52 +0700

    Update message.js

--- api/message.js
+++ api/message.js
@@ -1,16 +1,13 @@
 const { Client } = require('discord.js-selfbot-v13');
 const client = new Client();
 
-const token = process.env.DISCORD_TOKEN; // Token disimpan di Environment Vercel
+const token = process.env.DISCORD_TOKEN; // Menggunakan environment variable dari Vercel
 const channelId = '1283337144215277650'; // Ganti dengan ID channel
-const messageContent = 'Pesan otomatis dari selfbot'; // Ganti dengan pesan yang diinginkan
+const messageContent = 'Hallo MasD!!!'; // Ganti dengan pesan yang diinginkan
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
-    // Kirim pesan setiap 5 menit
-    setInterval(() => {
-        sendMessage();
-    }, 1000); // 300000 ms = 5 menit
+    sendMessage();
 });
 
 function sendMessage() {
@@ -24,4 +21,13 @@ function sendMessage() {
     }
 }
 
-client.login(token);
+// Fungsi handler Vercel untuk menerima request HTTP
+module.exports = async (req, res) => {
+    try {
+        await client.login(token);  // Login client Discord
+        res.status(200).json({ message: 'Pesan dikirim melalui Discord selfbot!' });
+    } catch (error) {
+        console.error('Gagal login ke Discord:', error);
+        res.status(500).json({ message: 'Gagal login ke Discord selfbot.' });
+    }
+};
