8173ff7407c1a1b35342c7d46eb8204e354b1c82
zentop <dedesans12@gmail.com>
2024-09-12 11:40:04 +0700

    Update message.js

--- api/message.js
+++ api/message.js
@@ -3,9 +3,9 @@ const { Client } = require('discord.js-selfbot-v13');
 const app = express();
 const client = new Client();
 
-const token = process.env.DISCORD_TOKEN;
-const channelId = '1283337144215277650';
-const messageContent = 'Pesan otomatis dari selfbot';
+const token = process.env.DISCORD_TOKEN; // Pastikan token ini diatur di environment variables platform
+const channelId = '1283337144215277650'; // Ganti dengan ID channel yang sesuai
+const messageContent = 'Pesan otomatis dari selfbot'; // Ganti dengan pesan yang diinginkan
 
 // Bot Discord
 client.on('ready', () => {
